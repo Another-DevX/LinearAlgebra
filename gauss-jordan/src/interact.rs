b67f6c58c0606a3a6e4101fc5866e57535cfd6b2
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-11-22 00:47:13 -0500

    feat: implement full gauss jordan elimination

--- gauss-jordan/src/interact.rs
+++ gauss-jordan/src/interact.rs
@@ -1,16 +1,21 @@
 pub fn get_size() {
-  println!("getting size")
+    println!("getting size")
 }
 
 pub fn create_matrix() {
-  println!("creating_matrix")
+    println!("creating_matrix")
 }
 
 pub fn print_matrix(matrix: &Vec<Vec<f32>>) {
-  for row in matrix {
-      for &val in row {
-          print!("{:5} ", val);
-      }
-      println!();
-  }
+    for row in matrix {
+        for (i, val) in row.iter().enumerate() {
+            if i == row.len() - 1 {
+                print!("| {:5} ", val);
+                continue;
+            }
+            print!("{:5} ", val);
+        }
+        println!();
+    }
+    println!();
 }
--- gauss-jordan/src/main.rs
+++ gauss-jordan/src/main.rs
@@ -4,29 +4,45 @@ use interact::print_matrix;
 fn main() {
     let mut matrix: Vec<Vec<f32>> = vec![
         vec![2., 4., 6., 18.],
-        vec![2., 5., 6., 24.],
+        vec![4., 5., 6., 24.],
         vec![3., 1., -2., 4.],
     ];
     print_matrix(&matrix);
-    println!();
     let matrix = jordan_gauss_elimination(matrix);
     print_matrix(&matrix);
 }
 
 fn jordan_gauss_elimination(mut matrix: Vec<Vec<f32>>) -> Vec<Vec<f32>> {
-    // Hallar pivote, en caso de que pivote distinto 1, dividir toda la fila
-    // Hallar divisores para hacer ceros en la columna del pivote
-    // Aplicar operaciones elementales a cada fila
 
     let n_rows = matrix.len();
     let m_cols = matrix[0].len();
 
-    if matrix[0][0] != 1. {
-        let divisor = matrix[0][0];
+    for diagonal in 0..n_rows {
+        print_matrix(&matrix);
+        if matrix[diagonal][diagonal] != 1. {
+            matrix = pivote(matrix, diagonal, diagonal);
+        }
         for i in 0..n_rows {
-            matrix[0][i] = matrix[0][i] / divisor;
+            if i != diagonal {
+                let factor = matrix[i][diagonal];
+                for j in 0..m_cols {
+                    matrix[i][j] -= matrix[diagonal][j]*factor ;
+                }
+            }
         }
     }
-
     matrix
 }
+
+fn pivote(mut matrix: Vec<Vec<f32>>, i: usize, j: usize) -> Vec<Vec<f32>> {
+    let divisor = matrix[i][j];
+    for k in 0..matrix[i].len() {
+        if matrix[i][k] == 0. {
+            continue;
+        }
+        matrix[i][k] = matrix[i][k] / divisor;
+    }
+    matrix
+}
+
+// matrix[i][j] = matrix[i][j] - matrix[0][j];
