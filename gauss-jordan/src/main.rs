cf359051b96acc782fc69f5501f13278d1570cb5
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-11-22 01:49:04 -0500

    feat: add degenerated ecuations

--- gauss-jordan/src/main.rs
+++ gauss-jordan/src/main.rs
@@ -3,9 +3,10 @@ use interact::print_matrix;
 
 fn main() {
     let mut matrix: Vec<Vec<f32>> = vec![
-        vec![2., 4., 6., 18.],
-        vec![4., 5., 6., 24.],
-        vec![3., 1., -2., 4.],
+        vec![1., 2., 3., 4.],
+        vec![2., 4., 6., 8.],
+        vec![3., 5., 7., 9.],
+        vec![6., 10., 14., 20.],
     ];
     print_matrix(&matrix);
     let matrix = jordan_gauss_elimination(matrix);
@@ -13,25 +14,48 @@ fn main() {
 }
 
 fn jordan_gauss_elimination(mut matrix: Vec<Vec<f32>>) -> Vec<Vec<f32>> {
-
     let n_rows = matrix.len();
     let m_cols = matrix[0].len();
 
     for diagonal in 0..n_rows {
+        if matrix[diagonal][diagonal] == 0. {
+            swap_with_nonzero_row(&mut matrix, diagonal);
+        }
         print_matrix(&matrix);
         if matrix[diagonal][diagonal] != 1. {
             matrix = pivote(matrix, diagonal, diagonal);
         }
         for i in 0..n_rows {
+            let mut degenerated = true;
             if i != diagonal {
                 let factor = matrix[i][diagonal];
                 for j in 0..m_cols {
-                    matrix[i][j] -= matrix[diagonal][j]*factor ;
+                    if matrix[i][j] != 0. && j == m_cols - 1 {
+                        panic!("inconsistent system")
+                    } else if matrix[i][j] != 0. {
+                        degenerated = false;
+                    }
+                    matrix[i][j] -= matrix[diagonal][j] * factor;
                 }
             }
         }
     }
+    matrix.sort_by(|a, b| {
+        b.iter()
+            .filter(|&&x| x != 0.)
+            .count()
+            .cmp(&a.iter().filter(|&&x| x != 0.).count())
+    });
     matrix
+}
+
+fn swap_with_nonzero_row(matrix: &mut Vec<Vec<f32>>, row_index: usize) {
+    for i in (row_index + 1)..matrix.len() {
+        if matrix[i][row_index] != 0. {
+            matrix.swap(row_index, i);
+            break;
+        }
+    }
 }
 
 fn pivote(mut matrix: Vec<Vec<f32>>, i: usize, j: usize) -> Vec<Vec<f32>> {
@@ -44,5 +68,3 @@ fn pivote(mut matrix: Vec<Vec<f32>>, i: usize, j: usize) -> Vec<Vec<f32>> {
     }
     matrix
 }
-
-// matrix[i][j] = matrix[i][j] - matrix[0][j];
